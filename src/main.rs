da99ffa57fd61010b974262dafcf394e070c3932
J Balint BIRO <j.balint.biro@gmail.com>
2017-12-29 22:04:23 +0100

    add argument parsing, takes program path from arguments now

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![feature(nll)]
 #![feature(conservative_impl_trait)]
 
+#[macro_use]
+extern crate clap;
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
@@ -13,6 +15,8 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use std::iter::FromIterator;
 
+use clap::{Arg, App};
+
 macro_rules! import_submodules {
 	($m:ident) => (mod $m; use $m::*;);
 	($m:ident, $($ms:ident),+) => (import_submodules!($m); import_submodules!($($ms),+););
@@ -21,10 +25,18 @@ macro_rules! import_submodules {
 import_submodules!(value, func, parse, internals, eval, bindings, builtins, read);
 
 fn main() {
-	let root_bindings =	default_root();
-	let program = "(print (* 2 (+ 1 2))) (print (quote 1 2 3)) (print (list (+ 1 1) (* 2 2)))";
+	let opts = App::new(crate_name!())
+					.version(crate_version!())
+					.author(crate_authors!("\n"))
+					.about(crate_description!())
+					.arg(Arg::with_name("INPUT")
+						.help("path to L3 program to interpret")
+						.required(true))
+					.get_matches();
 
-	for term in read_program(program).iter() {
+	let infile = opts.value_of("INPUT").unwrap();
+	let root_bindings = default_root();
+	for term in read_program_file(infile).iter() {
 		eval(term, root_bindings.clone());
 	}
 }
--- src/read.rs
+++ src/read.rs
@@ -5,6 +5,15 @@ pub fn read_program(inp: &str) -> Value {
 	parse_program(pairs)
 }
 
+pub fn read_program_file(file: &str) -> Value {
+	use pest::inputs::FileInput;
+	let pairs = L3Parser::parse(
+		Rule::program,
+		Rc::new(FileInput::new(file).unwrap_or_else(|e| panic!("{}", e))
+	)).unwrap_or_else(|e| panic!("{}", e));
+	parse_program(pairs)
+}
+
 pub fn read_list(inp: &str) -> Value {
 	let pairs = L3Parser::parse_str(Rule::list, inp).unwrap_or_else(|e| panic!("{}", e));
 	parse(pairs)
