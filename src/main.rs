364554553a656e2a97bca11f100e1439d835164b
J Balint BIRO <j.balint.biro@gmail.com>
2017-12-29 19:37:19 +0100

    remove commented out line

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,6 @@ import_submodules!(value, func, parse, internals, eval, bindings, builtins, read
 
 fn main() {
 	let root_bindings =	default_root();
-	//let program = "(fn hello () (print (* 2 (+ 1 2)))) (hello) (print (quote 1 2 3)) (print (list (+ 1 1) (* 2 2)))";
 	let program = "(print (* 2 (+ 1 2))) (print (quote 1 2 3)) (print (list (+ 1 1) (* 2 2)))";
 
 	for term in read_program(program).iter() {
